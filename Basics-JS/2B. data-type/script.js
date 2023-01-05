def2288a0c19f5277fbbba21090f16a4a2bb2f02
dzarurizkyy <112862854+dzarurizkyy@users.noreply.github.com>
2023-01-05 15:44:16 +0700

    Add files via upload

---
+++ Basics-JS/2B. data-type/script.js
@@ -0,0 +1,16 @@
+const name = "Balijan Uwais"; // String
+console.log(name);
+
+const mothersName = "Yani Purnawati"; // String
+console.log(mothersName);
+
+let age = 19; // Number
+console.log(age);
+age = 20;
+console.log(age);
+
+let maritalStatus = false; // Boolean
+console.log(maritalStatus);
+
+let totalChildren; // Undefined
+console.log(totalChildren);

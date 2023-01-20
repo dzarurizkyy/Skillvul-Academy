e67fca3f81f9084fa147baae540af6af43b82eaf
dzarurizkyy <112862854+dzarurizkyy@users.noreply.github.com>
2023-01-20 16:03:34 +0700

    Add files via upload

---
+++ Basics-JS/3B. assignment-operator/script.js
@@ -0,0 +1,20 @@
+let number = 10,
+    result;
+
+// Addition Assignment
+console.log(`${number} + 5 =`, number += 5);
+
+// Subtraction Assignment
+console.log(`${number} - 5 =`, number -= 5);
+
+// Multiplication Assignment
+console.log(`${number} x 5 =`, number *= 5);
+
+// Division Assignment
+console.log(`${number} / 5 =`, number /= 5);
+
+// Exponent Assignment
+console.log(`${number} ^ 5 =`, number **= 5);
+
+// Modulus Assignment
+console.log(`${number} % 5 =`, number %= 5);

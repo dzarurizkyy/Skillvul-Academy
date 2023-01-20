a82fcd8995fa2d67a400711e3defc8c78c1728c7
dzarurizkyy <112862854+dzarurizkyy@users.noreply.github.com>
2023-01-20 15:56:25 +0700

    Add files via upload

---
+++ Basics-JS/3A. arithmetic-operator/script.js
@@ -0,0 +1,31 @@
+let number1 = 10,
+    number2 = 3;
+
+// Addition
+console.log (`${number1} + ${number2} =`, number1 + number2);
+
+// Subtraction
+console.log (`${number1} - ${number2} =`, number1 - number2);
+
+// Multiplication
+console.log (`${number1} x ${number2} =`, number1 * number2);
+
+// Division
+console.log (`${number1} / ${number2} =`, number1 / number2);
+
+// Exponent
+console.log (`${number1} ^ ${number2} =`, number1 ** number2);
+
+// Modulus
+console.log (`${number1} % ${number2} =`, number1 % number2);
+
+let before = number1,
+    result;
+
+// Increment
+console.log (`${before} + 1 =`, ++number1);
+
+// Decrement
+console.log (`${before} - 1 =`, --before);
+
+

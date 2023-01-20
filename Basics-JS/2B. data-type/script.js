960eb72fb706f4fcec63765de77dacf04ddea287
dzarurizkyy <112862854+dzarurizkyy@users.noreply.github.com>
2023-01-20 15:52:38 +0700

    Add files via upload

---
+++ Basics-JS/2B. data-type/script.js
@@ -0,0 +1,19 @@
+// Declare Variable
+
+const name = "Charles";         // string
+const mothersName = "Gloria";   // string
+let age = 19;                   // number
+let married = false;            // boolean
+let children;                   // undefinied
+
+// Display in console
+
+console.log(name);
+console.log(mothersName);
+console.log(age);
+
+age = 19 + 2;
+console.log(age);
+
+console.log(married);
+console.log(children);

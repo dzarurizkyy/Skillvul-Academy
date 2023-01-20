3000010254be50aea0978c714a69a07fbf069144
dzarurizkyy <112862854+dzarurizkyy@users.noreply.github.com>
2023-01-20 16:13:48 +0700

    Add files via upload

---
+++ Basics-JS/3C. string-operator/script.js
@@ -0,0 +1,11 @@
+// String Concatenation
+
+let firstName = "Mark",
+    lastName  = "Zuckerberg";
+
+console.log(firstName + " " + lastName);
+
+let year = "born in ";
+year += 1984;
+
+console.log(year);

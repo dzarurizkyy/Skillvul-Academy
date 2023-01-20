7528055a096dfa34fbd4ef1c3ddb65499d80145c
dzarurizkyy <112862854+dzarurizkyy@users.noreply.github.com>
2023-01-20 15:49:46 +0700

    Add files via upload

---
+++ Basics-JS/2A. declare-variable/script.js
@@ -0,0 +1,12 @@
+//  Example of correctly written variable name
+    let name;
+    let phoneNumber;
+    let _email;
+    let $password;
+
+/* 
+    Example of incorrectly written variable name 
+    let full name; 
+    let @username;
+    let delete;
+*/
